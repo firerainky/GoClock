25899df87172820ebbd118d240d6919c8d5fd62b
zky <suchbalance@qq.com>
2020-08-10 06:14:41 +0800

    Initial Commit

---
+++ GoClock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoClock
+//
+//  Created by Zheng Kanyan on 2020/8/10.
+//  Copyright © 2020 Zheng Kanyan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
